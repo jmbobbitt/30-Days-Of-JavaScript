b641e5e3b89924b041fe703b027c4e9337722db5
Jenni B <71854518+jmbobbitt@users.noreply.github.com>
2021-01-18 16:18:23 -0500

    working on exercise 1, stopped at task 9

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,1 +1,17 @@
 // this is your main.js script
+
+//Exercise 1
+
+let challenge = '30 Days of JavaScript'
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(2))
+console.log(challenge.slice(2))
+console.log(challenge.includes('Script'))
+
+
+// Exercise 2
+
+// Exercise 3
