81821411fa7d3de3d4c1f6108f4962a715e12284
Jenni B <71854518+jmbobbitt@users.noreply.github.com>
2021-01-18 14:43:05 -0500

    completed day 1 challenge, continuing on to day2 challenge

--- 01_Day_Introduction/01_day_starter/variable.js
+++ 01_Day_Introduction/01_day_starter/variable.js
@@ -1,11 +1,11 @@
 // Declaring different variables of different data types
 
-let firstName = 'Asabeneh'    // first name of a person
-let lastName = 'Yetayeh'      // last name of a person
-let country = 'Finland'       // country
-let city = 'Helsinki'         // capital city
-let age = 100                 // age in years
-let isMarried = true
+let firstName = 'Jenni'    // first name of a person
+let lastName = 'Bobbitt'      // last name of a person
+let country = 'United States of America'       // country
+let city = 'Washington D.C.'         // capital city
+let age = 28                 // age in years
+let isMarried = false
 
 // Declaring variables with number values
 
